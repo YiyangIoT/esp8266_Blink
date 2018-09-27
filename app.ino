8ce54e55407a56921cdc4f6525e9479fb2e8ec47
Yiyang <chiangyiyang@gmail.com>
2018-09-27 22:47:11 +0800

    完成

--- app.ino
+++ app.ino
@@ -4,8 +4,6 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-
-  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
@@ -14,7 +12,4 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-
-  Serial.println(LED_BUILTIN);
-
 }
