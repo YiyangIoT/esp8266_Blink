cd387da47f364b0e4cdff2f79e88301c3f70fbec
Yiyang <chiangyiyang@gmail.com>
2018-09-27 22:45:49 +0800

    使用Serial輸出LED_BUILTIN值

--- app.ino
+++ app.ino
@@ -1,7 +1,10 @@
 
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
@@ -10,4 +13,7 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+
+  Serial.println(LED_BUILTIN);
+
 }
